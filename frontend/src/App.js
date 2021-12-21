63c2d1e7f394c6fbd136f8d1c0c3f11982bed710
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-21 14:28:20 -0600

    add mapbox to display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
   return (
     <div className="App">
-      <h1>hoi</h1>
-    </div>
+    <ReactMapGL
+    {...viewport}
+    onViewportChange={nextViewport => setViewport(nextViewport)}
+  />
+  </div>
   );
 }
 
