75cd5024f90791e8b0f395df1e442299ddffa208
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-21 09:58:25 -0600

    hook up database to backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express"); 
 const mongoose = require("mongoose"); 
 const app = express(); 
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Mongo connected")
+}).catch((err) => console.log(err)
+)
 
 app.listen(5000, ()=> {
     console.log("Backend server is running!")
