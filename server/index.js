b759017bf6a78080ba222886995538415ba4d61a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-21 10:37:36 -0600

    build pin route

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose"); 
 const app = express(); 
 const dotenv = require('dotenv');
+const pinRoute = require("./routes/pins")
+
 dotenv.config(); 
+app.use(express.json())
+
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Mongo connected")
 }).catch((err) => console.log(err)
 )
 
+app.use("/api/pins", pinRoute); 
 
 app.listen(5000, ()=> {
     console.log("Backend server is running!")
