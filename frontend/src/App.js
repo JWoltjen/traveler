1e8f258082329f7fb744ba03b860d1d0038da481
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-21 14:38:54 -0600

    orient marker to paris

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
+    width: "100vw",
+    height: "100vh",
+    latitude: 48.85,
+    longitude: 2.294694,
     zoom: 8
   });
   return (
     <div className="App">
-      hellov
     <ReactMapGL
     {...viewport}
     mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
     onViewportChange={nextViewport => setViewport(nextViewport)}
-  />
+  >
+    <Marker
+      latitude={48.85}
+      longitude={2.294694}
+      offsetLeft={-20}
+      offsetTop={-10}
+    >
+      <div>
+        You are here
+      </div>
+    </Marker>
+  </ReactMapGL>
   </div>
   );
 }
