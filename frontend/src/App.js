4bdd0f44dd1028c6ca25299a006ebc71560690cd
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-23 11:16:27 -0600

    send new pin information to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ function App() {
   const [pins, setPins] = useState([])
   const [currentPlaceId, setCurrentPlaceId] = useState(null)
   const [newPlace, setNewPlace] = useState(null)
+  const [title, setTitle] = useState(null)
+  const [desc, setDesc] = useState(null)
+  const [rating, setRating] = useState(0)
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -43,6 +46,26 @@ function App() {
       lng: lng,
     })
     console.log(newPlace)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser, 
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat, 
+      long: newPlace.lng
+    }
+
+    try {
+      const res = await axios.post("/pins", newPin)
+      setPins([...pins, res.data])
+      setNewPlace(null)
+    } catch (err) {
+      console.log(err)
+    }
   }
   
   return (
@@ -106,13 +129,19 @@ function App() {
           onClose={()=>setNewPlace(null)}
         >
           <div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label>Title</label>
-              <input placeholder="enter a title"/>
+              <input 
+                placeholder="enter a title" 
+                onChange={(e)=>setTitle(e.target.value)}
+              />
               <label>Review</label>
-              <textarea placeholder="tell us about this place"/>
+              <textarea 
+                placeholder="tell us about this place"
+                onChange={(e)=>setDesc(e.target.value)}
+              />
               <label>Rating</label>
-              <select>
+              <select onChange={(e)=> setRating(e.target.value)}>
                 <option value='1'>1</option>
                 <option value='2'>2</option>
                 <option value='3'>3</option>
