0cf0248cbbc6cbf80520768176218f157623c7a9
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-24 15:47:32 -0600

    create login functionality

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,45 @@
-import "./Login.css"
+import "./Login.css"; 
+import { Room, Cancel } from "@material-ui/icons"; 
+import { useState, useRef } from 'react'; 
+import axios from "axios";
 
-export default function Login(setShowLogin) {
+function Login({setShowLogin}) {
+
+    const [error, setError] = useState(false)
+    const nameRef = useRef()
+    const passwordRef = useRef()
+
+    const handleSubmit = async(e) => {
+        e.preventDefault(); 
+        const newUser = {
+            username: nameRef.current.value, 
+            password: passwordRef.current.value,
+        }; 
+        try {
+            await axios.post("/users/login", newUser); 
+            setError(false)
+        } catch (err) {
+            setError(true)
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="loginContainer">
+            <div className="logo">
+                <Room/> 
+                Traveler
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="username" ref={nameRef}/>
+                <input type="password" placeholder="password" ref={passwordRef}/>
+                <button className="loginBtn">Login</button>
+                {error && (
+                    <span className="failure">Login unsuccessful</span>
+                )}
+            </form>
+            <Cancel className="loginCancel" onClick={()=>setShowLogin(false)}/>
         </div>
     )
 }
+
+export default Login
