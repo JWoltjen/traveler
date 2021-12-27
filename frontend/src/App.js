690d4c2a3f33c19e9024370e455c030d0b938e0e
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-27 11:02:21 -0600

    add user to local storage on login, remove on logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Register from '../src/components/Register'
 import Login from './components/Login';
 
 function App() {
+  const myStorage = window.localStorage; 
   const [currentUser, setCurrentUser] = useState(null)
   const [pins, setPins] = useState([])
   const [currentPlaceId, setCurrentPlaceId] = useState(null)
@@ -162,7 +163,7 @@ function App() {
         </div>
       )}
       {showRegister &&  <Register setShowRegister={setShowRegister}/>}
-      {showLogin && <Login setShowLogin={setShowLogin}/>}
+      {showLogin && <Login setShowLogin={setShowLogin} myStorage={myStorage} setCurrentUser={setCurrentUser}/>}
     </ReactMapGL>
   </div>
   );
